b7cccbc4c3a14307dfa8b21de76adc094201d753
Sara <sara@inkmoon.net>
2024-10-07 12:37:38 +0100

    Global nav established and shows active link.

---
+++ src/app/(components)/dashboard/layout.tsx
@@ -0,0 +1,11 @@
+import MainNav from "@/app/ui/mainnav";
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+  <>
+  <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+        Test
+    </div>
+  </>
+  );
+}
