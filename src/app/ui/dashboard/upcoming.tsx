be5164cc8dd66155a80aa725678f3f089b6a46fb
Sara <sara@inkmoon.net>
2024-10-07 22:17:05 +0100

    Dashboard with mock elements fully implemented.

--- src/app/ui/dashboard/upcoming.tsx
+++ src/app/ui/dashboard/upcoming.tsx
@@ -1,3 +1,9 @@
+import {Ubuntu} from 'next/font/google';
+
+const ubuntu = Ubuntu({
+    weight: '400',
+    subsets: ['latin'],
+  })
 
 // Double-check format of date and create map for fetched bookings data.
 
@@ -8,7 +14,7 @@ export default function Upcoming () {
             <div className="flex flex-row text-[black] mb-[25px] px-5 py-2.5 rounded-[10px] shadow-[0_4px_8px_0_rgba(0,0,0,0.2),0_6px_20px_0_rgba(0,0,0,0.19)]" style={{background: 'linear-gradient(11deg, rgba(255, 103, 43, 1) 0%, rgba(251, 173, 16, 1) 100%)'}}>
                 <span className="text-xl w-[26%] mr-6 border-r-[black] border-r border-solid"><span className="text-[35px]">15 </span>OCT</span>
                 <div className="flex flex-col text-center w-3/5">
-                    <span className="uppercase mx-0 my-1.5">Meowler's Bar</span>
+                    <span className={`uppercase mx-0 my-1.5 ${ubuntu.className}`}>Meowler's Bar</span>
                     <span className="text-sm italic">18:30</span>
                 </div>
             </div>
