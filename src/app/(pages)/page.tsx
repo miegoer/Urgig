28e421b95f09f96fb70e56bb4c10453b21e78e71
Damir Lončarić <damir.loncaric987@gmail.com>
2024-10-07 22:36:31 +0200

    moving main page.tsx, layout.tsx and globals.css to /(pages)/folder

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,23 +1,21 @@
-import "@/app/globals.css";
+import "./globals.css";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 // This should be the front page -- before login and etc.
-
 
 export default function Home() {
   return (
     <div>
       <h1>MAIN PAGE</h1>
       <Link href="/sign-up">CREATE AN ACCOUNT</Link>
-      <br/>
+      <br />
       <Link href="/sign-in"> LOGIN </Link>
     </div>
   );
 }
 
 // import clsx from 'clsx';
-
 
 // export default function navBarA () {
 //     const pathname = usePathname();
