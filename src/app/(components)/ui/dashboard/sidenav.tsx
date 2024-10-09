1f906821c9a979c4f8f1628bdef5a09e1935d172
Damir Lončarić <damir.loncaric987@gmail.com>
2024-10-09 21:09:18 +0200

    moved public folder into root folder so it's accessible like it was meant to be

--- src/app/(components)/ui/dashboard/sidenav.tsx
+++ src/app/(components)/ui/dashboard/sidenav.tsx
@@ -1,35 +1,35 @@
-'use client'
+"use client";
 
-import Link from 'next/link';
-import clsx from 'clsx';
-import { usePathname } from 'next/navigation';
+import Link from "next/link";
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
 import Image, { StaticImageData } from "next/image";
-import Dashboard from '@/public/dashboard-icon.png';
-import Payments from '@/public/earnings-icon.png';
-import Settings from '@/public/settings-icon.png';
-import Calendar from '@/public/calendar-icon.png';
-import Analytics from '@/public/analytics-icon.png';
-import Create from '@/public/create-icon.png';
-import Account from '@/public/profile-icon.png';
-import Contracts from '@/public/contracts-icon.png';
-import './dashboard-ui.css';
+// import Dashboard from "/dashboard-icon.png";
+// import Payments from "/earnings-icon.png";
+// import Settings from "/settings-icon.png";
+// import Calendar from "/calendar-icon.png";
+// import Analytics from "/analytics-icon.png";
+// import Create from "/create-icon.png";
+// import Account from "/profile-icon.png";
+// import Contracts from "/contracts-icon.png";
+import "./dashboard-ui.css";
 
-const baseRoute = '/dashboard'
+const baseRoute = "/dashboard";
 
 interface sideNavLink {
-  icon: StaticImageData;
+  icon: string;
   href: string;
   alt: string;
 }
 
 const sideNavLinks: sideNavLink[] = [
-  { icon: Dashboard, href: `${baseRoute}`, alt: 'Dashboard Icon' },
-  { icon: Create, href: `${baseRoute}/create`, alt: 'Create Icon' },
-  { icon: Payments, href: `${baseRoute}/payments`, alt: 'Payments Icon' },
-  { icon: Analytics, href: `${baseRoute}/analytics`, alt: 'Analytics Icon' },
-  { icon: Contracts, href: `${baseRoute}/contracts`, alt: 'Contracts Icon' },
-  { icon: Account, href: `${baseRoute}/account`, alt: 'Account Icon' },
-  { icon: Settings, href: `${baseRoute}/settings`, alt: 'Settings Icon' }
+  { icon: "/dashboard-icon.png", href: `${baseRoute}`, alt: "Dashboard Icon" },
+  { icon: "/create-icon.png", href: `${baseRoute}/create`, alt: "Create Icon" },
+  { icon: "/earnings-icon.png", href: `${baseRoute}/payments`, alt: "Payments Icon" },
+  { icon: "/analytics-icon.png", href: `${baseRoute}/analytics`, alt: "Analytics Icon" },
+  { icon: "/contracts-icon.png", href: `${baseRoute}/contracts`, alt: "Contracts Icon" },
+  { icon: "/profile-icon.png", href: `${baseRoute}/account`, alt: "Account Icon" },
+  { icon: "/settings-icon.png", href: `${baseRoute}/settings`, alt: "Settings Icon" },
 ]; // Icons in the Dashboard's SideNav. Not yet linked to pages.
 
 export default function SideNav() {
@@ -38,15 +38,25 @@ export default function SideNav() {
     <div className="col-[col-start_1_/_span_1] row-[2_/_span_8] w-[55px] shadow-[0px_0px_0px_#272525] ml-5 mt-0 pl-2.5 pr-0 pt-0 pb-[35px] rounded-[20px]">
       {sideNavLinks.map((link) => (
         <Link key={link.alt} href={link.href}>
-        <Image src={link.icon} alt={link.alt} width={35} height={35} className={clsx("my-[42px] p-[5px] transition-all duration-200", {'bg-[#3525de] rounded-[10px]' : pathname === link.href}, 'hover:bg-[#3525de] hover:rounded-[10px] hover:h-[35px]')}/>
-        </Link> 
+          <Image
+            src={link.icon}
+            alt={link.alt}
+            width={35}
+            height={35}
+            className={clsx(
+              "my-[42px] p-[5px] transition-all duration-200",
+              { "bg-[#3525de] rounded-[10px]": pathname === link.href },
+              "hover:bg-[#3525de] hover:rounded-[10px] hover:h-[35px]"
+            )}
+          />
+        </Link>
       ))}
-{/* <Link key={link.name} href={link.href} className={clsx(
+      {/* <Link key={link.name} href={link.href} className={clsx(
                 'text-[#dcff58] p-3 uppercase text-[12px] mx-[24px] my-[6px] tracking-[3px] transition-all duration-200',
                 {'text-[#ffc037]': pathname === link.href},
                 'hover:bg-[#3525de] hover:rounded-[5px]')}>
                 {link.name}
             </Link> */}
     </div>
-  )
+  );
 }
--- src/app/(pages)/(main)/myprofile/layout.tsx
+++ src/app/(pages)/(main)/myprofile/layout.tsx
@@ -1,75 +1,141 @@
-'use client'
+"use client";
 
-import mockUsers from '@/mockData/user';
-import Link from 'next/link';
-import DJFrankenstein from '@/public/mockUsers/DJFrankenstein.png'
+import mockUsers from "@/mockData/user";
+import Link from "next/link";
+const DJFrankenstein = "/mockUsers/DJFrankenstein.png";
 import Image from "next/image";
-import Back from '@/public/back-icon.png';
-import StartChat from '@/public/startchat-icon.png';
-import Connect from '@/public/connect-icon.png';
-import Spotify from '@/public/spotify-icon.png';
-import Instagram from '@/public/ig-icon.png';
-import Tiktok from '@/public/tiktok-icon.png';
-import Youtube from '@/public/youtube-icon.png';
-import Location from '@/public/location-icon.png';
-import {Ubuntu} from 'next/font/google';
-import { useRouter } from 'next/navigation';
-import clsx from 'clsx';
-import { usePathname } from 'next/navigation';
+const Back = "/back-icon.png";
+const StartChat = "/startchat-icon.png";
+const Connect = "/connect-icon.png";
+const Spotify = "/spotify-icon.png";
+const Instagram = "/ig-icon.png";
+const Tiktok = "/tiktok-icon.png";
+const Youtube = "/youtube-icon.png";
+const Location = "/location-icon.png";
+import { Ubuntu } from "next/font/google";
+import { useRouter } from "next/navigation";
+import clsx from "clsx";
+import { usePathname } from "next/navigation";
 
 const ubuntu = Ubuntu({
-    weight: '400',
-    subsets: ['latin'],
-  })
+  weight: "400",
+  subsets: ["latin"],
+});
 
-
-const baseRoute = '/myprofile'
+const baseRoute = "/myprofile";
 
 interface profileLink {
-    name: string;
-    href: string;
-  }
+  name: string;
+  href: string;
+}
 
 const profileLinks: profileLink[] = [
-    { name: 'Bio', href: `${baseRoute}`}, { name: 'Fan Base', href: `${baseRoute}/fanbase` }, { name: 'Events', href: `${baseRoute}/events` }, { name: 'Media', href: `${baseRoute}/media` }, { name: 'Q&A', href: `${baseRoute}/qa` }
-]
+  { name: "Bio", href: `${baseRoute}` },
+  { name: "Fan Base", href: `${baseRoute}/fanbase` },
+  { name: "Events", href: `${baseRoute}/events` },
+  { name: "Media", href: `${baseRoute}/media` },
+  { name: "Q&A", href: `${baseRoute}/qa` },
+];
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    const router = useRouter();
-    const pathname = usePathname();
-    return (
-        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(12, 1fr)', gridTemplateRows: 'auto 1fr' }}>
-            <div style={{ gridColumn: '1 / span 3', gridRow: '2 / span 10' }} className="h-[660px] w-[530px] relative">
-                <Image src={DJFrankenstein} alt="mock profile photo" className="h-[100%] w-[100%] shadow-[0_4px_8px_0_rgba(0,0,0,0.2),0_6px_20px_0_rgba(0,0,0,0.19)] -ml-5 rounded-r-[50%]"/>
-                <div className="h-[80px] px-5 flex flex-col top-[82%] inline-flex z-5 absolute bg-[rgba(0,0,0,0.7)]">
-                <div className={`z-10 py-2.5 rounded-[3px] tracking-[1.5px] text-[white] ${ubuntu.className} text-2xl`}>
-                    {mockUsers[0].name}
-                </div>
-                <span className="text-sm italic -mt-2 text-center ${ubuntu.className}">{mockUsers[0].location}</span>
-                </div>
-                <Image src={StartChat} alt="Chat button" className="w-[28px] h-[28px] absolute top-[40px] left-[78%] hover:scale-[1.2]"/>
-                <Image src={Connect} alt="Connect button" className="w-[28px] h-[28px] absolute top-[120px] left-[90%] hover:scale-[1.2]"/>
-                <Image src={Spotify} alt="Spotify button" className="w-[28px] h-[28px] absolute top-[220px] left-[96%] hover:scale-[1.2]"/>
-                <Image src={Instagram} alt="Instagram button" className="w-[28px] h-[28px] absolute top-[330px] left-[98%] hover:scale-[1.2]"/>
-                <Image src={Youtube} alt="YouTube button" className="w-[28px] h-[28px] absolute top-[440px] left-[95%] hover:scale-[1.2]"/>
-                <Image src={Tiktok} alt="Tiktok button" className="w-[30px] h-[30px] absolute top-[545px] left-[85%] hover:scale-[1.2]"/>
-            </div>
-            <div style={{ gridColumn: '4 / span 4', gridRow: '1 / span 4' }} className="h-[660px] w-[480px]">
-            {children}
-            </div>
-            <div style={{ gridColumn: '8 / span 3', gridRow: '2 / span 8' }} className="w-[160px] ml-[70px]">
-            {profileLinks.map((link) => (
-                <Link href={link.href}>
-                    <div className={clsx(
-                        "w-[100%] my-[60px] p-4 rounded-[2px] text-center tracking-[3px] text-[white] text-xs border border-solid border-[white] uppercase",
-                        { 'bg-[white] text-[#1b1b25]': pathname === link.href },
-                        'hover:bg-[white] hover:text-[black] hover:scale-110'
-                        )}>
-                    {link.name}
-                    </div>    
-                </Link>          
-            ))}
-            </div>
+  const router = useRouter();
+  const pathname = usePathname();
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(12, 1fr)",
+        gridTemplateRows: "auto 1fr",
+      }}
+    >
+      <div
+        style={{ gridColumn: "1 / span 3", gridRow: "2 / span 10" }}
+        className="h-[660px] w-[530px] relative"
+      >
+        <Image
+          src={DJFrankenstein}
+          width={250}
+          height={250}
+          alt="mock profile photo"
+          className="h-[100%] w-[100%] shadow-[0_4px_8px_0_rgba(0,0,0,0.2),0_6px_20px_0_rgba(0,0,0,0.19)] -ml-5 rounded-r-[50%]"
+        />
+        <div className="h-[80px] px-5 flex flex-col top-[82%] inline-flex z-5 absolute bg-[rgba(0,0,0,0.7)]">
+          <div
+            className={`z-10 py-2.5 rounded-[3px] tracking-[1.5px] text-[white] ${ubuntu.className} text-2xl`}
+          >
+            {mockUsers[0].name}
+          </div>
+          <span className="text-sm italic -mt-2 text-center ${ubuntu.className}">
+            {mockUsers[0].location}
+          </span>
         </div>
-    )
-  }
+        <Image
+          src={StartChat}
+          width={25}
+          height={25}
+          alt="Chat button"
+          className="w-[28px] h-[28px] absolute top-[40px] left-[78%] hover:scale-[1.2]"
+        />
+        <Image
+          src={Connect}
+          width={25}
+          height={25}
+          alt="Connect button"
+          className="w-[28px] h-[28px] absolute top-[120px] left-[90%] hover:scale-[1.2]"
+        />
+        <Image
+          src={Spotify}
+          width={25}
+          height={25}
+          alt="Spotify button"
+          className="w-[28px] h-[28px] absolute top-[220px] left-[96%] hover:scale-[1.2]"
+        />
+        <Image
+          src={Instagram}
+          width={25}
+          height={25}
+          alt="Instagram button"
+          className="w-[28px] h-[28px] absolute top-[330px] left-[98%] hover:scale-[1.2]"
+        />
+        <Image
+          src={Youtube}
+          width={25}
+          height={25}
+          alt="YouTube button"
+          className="w-[28px] h-[28px] absolute top-[440px] left-[95%] hover:scale-[1.2]"
+        />
+        <Image
+          src={Tiktok}
+          width={25}
+          height={25}
+          alt="Tiktok button"
+          className="w-[30px] h-[30px] absolute top-[545px] left-[85%] hover:scale-[1.2]"
+        />
+      </div>
+      <div
+        style={{ gridColumn: "4 / span 4", gridRow: "1 / span 4" }}
+        className="h-[660px] w-[480px]"
+      >
+        {children}
+      </div>
+      <div
+        style={{ gridColumn: "8 / span 3", gridRow: "2 / span 8" }}
+        className="w-[160px] ml-[70px]"
+      >
+        {profileLinks.map((link) => (
+          <Link href={link.href}>
+            <div
+              className={clsx(
+                "w-[100%] my-[60px] p-4 rounded-[2px] text-center tracking-[3px] text-[white] text-xs border border-solid border-[white] uppercase",
+                { "bg-[white] text-[#1b1b25]": pathname === link.href },
+                "hover:bg-[white] hover:text-[black] hover:scale-110"
+              )}
+            >
+              {link.name}
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
