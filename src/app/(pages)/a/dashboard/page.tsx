a006a62d87e77cabb5544409dcb2d810ce17c8ce
Aidan <aidanl0919@gmail.com>
2024-10-09 11:35:24 +0100

    resolve merge confilcts

--- src/app/(pages)/a/dashboard/page.tsx
+++ src/app/(pages)/a/dashboard/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "@/app/(pages)/globals.css";
 
 export default function Dashboard() {
@@ -8,16 +7,3 @@ export default function Dashboard() {
     </main>
   );
 }
-=======
-import '@/app/globals.css';
-
-export default function Dashboard() {
-    return (
-        <main>
-            <h1 className={`mb-4 text-xl md:text-2xl`}>
-            Dashboard
-            </h1>
-        </main>
-    )
-  }
->>>>>>> main
