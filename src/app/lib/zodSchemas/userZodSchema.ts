c3d20d801e11a62854ec5c5e8faba90388529a0b
skirms <jagodic77@gmail.com>
2024-10-10 09:41:52 +0200

    fixed accidently downgrading ZodUsersFile

--- src/app/lib/zodSchemas/userZodSchema.ts
+++ src/app/lib/zodSchemas/userZodSchema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 // Define related ZodSchemas like ProfileDetails, Settings, etc.
-const ProfileDetailsZodSchema = z.object({
+export const ProfileDetailsZodSchema = z.object({
   profilePicture: z.string().optional(),
   aboutMe: z.string().optional(),
   selectedVideo: z.string().optional(),
@@ -20,23 +20,22 @@ const ProfileDetailsZodSchema = z.object({
   genre: z.array(z.string()).optional(),
 });
 
-const SettingsZodSchema = z.object({
+export const SettingsZodSchema = z.object({
   // Add fields for settings
 });
 
-const StatisticsZodSchema = z.object({
-  profileViews: z.number().optional(),
-  offersGot: z.number().optional(),
-  offersAcccepted: z.number().optional(),
+export const StatisticsZodSchema = z.object({
+  profileViews: z.number().int().optional(),
+  offersGot: z.number().int().optional(),
+  offersAcccepted: z.number().int().optional(),
   income: z.number().optional(),
-  avgCapacity: z.number().optional(),
-  totalAtendees: z.number().optional(),
-  totalEvents: z.number().optional(),
+  avgCapacity: z.number().int().optional(),
+  totalAtendees: z.number().int().optional(),
+  totalEvents: z.number().int().optional(),
 });
 
 export const UserZodSchema = z.object({
   _id: z.string(),
-  // added stageName
   stageName: z.string().optional(),
   email: z.string().email(),
   typeOfAccount: z.string(),
@@ -57,59 +56,3 @@ export const UserZodSchema = z.object({
   pastEvents: z.array(z.string()).optional(),
   upcomingEvents: z.array(z.string()).optional(),
 });
-
-// import { z } from "zod";
-
-// // Define related ZodSchemas like ProfileDetails, Settings, etc.
-// const ProfileDetailsZodSchema = z.object({
-//   profilePicture: z.string().optional(),
-//   aboutMe: z.string().optional(),
-//   selectedVideo: z.string().optional(),
-//   socialLinks: z
-//     .object({
-//       twitter: z.string().url().optional(),
-//       facebook: z.string().url().optional(),
-//       youtube: z.string().url().optional(),
-//       instagram: z.string().url().optional(),
-//       spotify: z.string().url().optional(),
-//       tiktok: z.string().url().optional(),
-//     })
-//     .optional(),
-//   unAvailableDates: z.array(z.date()).optional(),
-//   bannerPicture: z.string().optional(),
-//   genre: z.array(z.string()).optional(),
-// });
-
-// const SettingsZodSchema = z.object({
-//   // Add fields for settings
-// });
-
-// const StatisticsZodSchema = z.object({
-//   profileViews: z.number().optional(),
-//   offersGot: z.number().optional(),
-//   offersAcccepted: z.number().optional(),
-//   income: z.number().optional(),
-//   avgCapacity: z.number().optional(),
-//   totalAtendees: z.number().optional(),
-//   totalEvents: z.number().optional(),
-// });
-
-// export const UserZodSchema = z.object({
-//   _id: z.string(),
-//   email: z.string().email(),
-//   typeOfAccount: z.string(),
-//   name: z.string().optional(),
-//   contactNumber: z.string().optional(),
-//   password: z.string().optional(),
-//   dateOfBirth: z
-//     .string()
-//     .transform((str) => new Date(str)) // Transform string to Date
-//     .refine((date) => !isNaN(date.getTime()), { message: "Invalid date format" })
-//     .optional(), // Validate that it's a valid date
-//   location: z.string().optional(),
-//   settings: SettingsZodSchema.optional(),
-//   profileDetails: ProfileDetailsZodSchema.optional(),
-//   statistics: StatisticsZodSchema.optional(),
-//   pastEvents: z.array(z.string()).optional(),
-//   upcomingEvents: z.array(z.string()).optional(),
-// });
