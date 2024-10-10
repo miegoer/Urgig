1b366a565db76a3fd02945fe7aed31e4ea0b5c1f
Damir Lončarić <damir.loncaric987@gmail.com>
2024-10-10 10:30:11 +0200

    fixed schema tests

--- src/app/lib/mongoDB/schemas/userSchema.ts
+++ src/app/lib/mongoDB/schemas/userSchema.ts
@@ -1,7 +1,7 @@
-import { Document, Schema } from 'mongoose';
-import { StatisticsSchema } from './statisticsSchema';
-import { ProfileSchema } from './profileSchema';
-import { User } from '@/types/user';
+import { Document, Schema, ValidatorProps } from "mongoose";
+import { StatisticsSchema } from "./statisticsSchema";
+import { ProfileSchema } from "./profileSchema";
+import { User } from "@/types/user";
 
 const SettingsSchema: Schema = new Schema({});
 
@@ -15,20 +15,29 @@ export const UserSchema: Schema = new Schema({
   _id: { type: String, required: true },
   typeOfAccount: { type: String, required: true },
   // added stageName
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v: string) {
+        return /^\S+@\S+\.\S+$/.test(v); // Simple regex to check email format
+      },
+      message: (props: ValidatorProps) => `${props.value} is not a valid email!`,
+    },
+  },
   stageName: { type: String, required: false },
-  email: { type: String, required: true },
   name: { type: String, required: false },
   contactNumber: { type: String, required: false },
   password: { type: String, required: false, select: false }, // Ensure password is excluded by default. You won’t get the password field unless you specifically request it in a query using .select("+password").
   dateOfBirth: { type: Date, required: false },
   location: { type: String, required: false },
-  settings: { type: [SettingsSchema], ref: 'Settings', required: false },
+  settings: { type: [SettingsSchema], ref: "Settings", required: false },
   profileDetails: {
     type: [ProfileSchema],
-    ref: 'ProfileDetails',
+    ref: "ProfileDetails",
     required: false,
   },
-  statistics: { type: [StatisticsSchema], ref: 'Statistics', required: false },
+  statistics: { type: [StatisticsSchema], ref: "Statistics", required: false },
   pastEvents: { type: [String], required: false }, //preselected values
   upcomingEvents: { type: [String], required: false }, //preselected values
 });
