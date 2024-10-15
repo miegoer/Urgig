daadbd5a175efeef0951405b6ce942506ab65a1a
Sara <sara@inkmoon.net>
2024-10-15 10:57:18 +0100

    Changes to booking request

--- src/app/(components)/ui/a/booknow.tsx
+++ src/app/(components)/ui/a/booknow.tsx
@@ -24,11 +24,13 @@ interface SelectedEvent {
     value: Event,
     label: string,
 }
+// This type is a readable format for the dropdown menu as it requires a label property.
 
 interface SelectedDate {
     value: Date,
     label: string,
 }
+// Ditto, but for dates.
 
 const noOptionsMessage = () => 'No Event Attached';
 
@@ -37,38 +39,51 @@ export const BookNow: React.FC<BookNowProps> = ({ setOpenBooking }) => {
     const initialState:Booking = {
         _id: '',
         name: '',
-        // link?: string;
         location: '',
         offer: 0,
         sets: [],
-        genre: [],
+        expectedGenre: [],
         maxCapacity: 0,
         status: 'pending',
         bookingPromoterId: '',
         bookingArtistId: '',
         bookingEventId: '',
         link: ''
-      };
+      }; // Initial state for empty booking request
 
     const initialSet: Set = {
         _id: '',
-        date: Date,
+        date: new Date,
         setTimeStart: '',
         setTimeEnd: ''
-    }
-      
+    } // Initial state for empty set, which will be added to 'sets' above
 
     const [bookingData, setBookingData] = useState(initialState);
+    // Empty booking request
 
-    const [offer, setOffer] = useState<number>(0);
-    const [payType, setPayType] = useState<string>('');
     const [selectedEvent, setSelectedEvent] = useState<SelectedEvent | null>(null);
-    const [startTime, setStartTime] = useState<string>('');
-    const [endTime, setEndTime] = useState<string>('');
-    const [comments, setComments] = useState<string>('');
-    const [setDetails, setSetDetails] = useState<Set>({})
+    // The above is a separate type which includes a label for the dropdown menu.
 
     const [dates, setDates] = useState<{ value: string; label: string }[]>([]);
+    // All date options for the selected event above, for dropdown menu.
+
+    const [offer, setOffer] = useState<number>(0);
+    // sets offer amount
+    
+    const [travelExpenses, setTravelExpenses] = useState<boolean>(false);
+    // travel exp paid or unpaid
+
+    const [startTime, setStartTime] = useState<string>('');
+    // sets start time of set
+
+    const [endTime, setEndTime] = useState<string>('');
+    // sets end time of set
+
+    const [setDetails, setSetDetails] = useState<Set>(initialSet);
+    // pulls all set details
+
+    const [comments, setComments] = useState<string>('');
+    // IGNORE
 
     const events:SelectedEvent[] = mockEvents.map((event) => ({
         value: event,
@@ -76,32 +91,43 @@ export const BookNow: React.FC<BookNowProps> = ({ setOpenBooking }) => {
       }));
  
     const chooseEvent = (event: SelectedEvent) => {
+        // Triggered after selecting an event
         setSelectedEvent(event);
-
         const formattedDate = event.value.date.toLocaleDateString('en-GB', {
             day: 'numeric',
             month: 'long',
             year: 'numeric'
           });
-
-        setDates([{value: formattedDate, label: formattedDate}]);
-
+        setDates([{value: formattedDate, label: formattedDate}]); // Fills dropdown menu with corresponding dates
         setBookingData((prevData) => ({
             ...prevData,
             name: event.value.name,
             location: event.value.location,
             maxCapacity: event.value.maxCapacity,
             bookingPromoterId: event.value.promoterId,
-            genre: event.value.genre
-        }));
+            expectedGenre: event.value.genre
+        })); // Sets booking data with info already embedded in the event data
     }
 
     const chooseDate = (date: SelectedDate) => {
+        // Triggered after selecting a date
         setBookingData((prevData) => ({
             ...prevData,
             date: date.value,
         }));
     }
+
+    // const parseDateString = (dateString:string) => {
+    //     const [day, month, year] = dateString.split(' ');
+    //     const months = {
+    //       January: 0, February: 1, March: 2, April: 3,
+    //       May: 4, June: 5, July: 6, August: 7,
+    //       September: 8, October: 9, November: 10, December: 11,
+    //     };
+    //     return new Date(year, months[month], day);
+    // }; 
+    // Converts dates in string-form back to a date type. Unclear if needed.
+
 
     const handleOfferInput = (event:ChangeEvent<HTMLInputElement>) => {
         setOffer(Number(event.target.value));
@@ -109,47 +135,45 @@ export const BookNow: React.FC<BookNowProps> = ({ setOpenBooking }) => {
             ...prevData,
             offer: Number(event.target.value),
           }));
-    }
-
-    const handleTypeInput = (val:string) => {
-        setPayType(val)
-    }
+        }
 
     const handleStartInput = (event: ChangeEvent<HTMLInputElement>) => {
-        setBookingData((prevData) => ({
-          ...prevData,
-          sets: prevData.sets[0].map((set, index) => 
-            index === 0 
-              ? { ...set, setTimeStart: event.target.value } 
-              : set
-          ),
-        }));
+        setStartTime(event.target.value)
+        setSetDetails((prevData) => ({
+            ...prevData,
+            setTimeStart: event.target.value
+        }))
       };
-      
 
     const handleEndInput = (event:ChangeEvent<HTMLInputElement>) => {
+        setEndTime(event.target.value)
+        setSetDetails((prevData) => ({
+            ...prevData,
+            setTimeEnd: event.target.value
+        }))
+    }
+
+    const handleWebsiteInput = (event:ChangeEvent<HTMLInputElement>) => {
         setBookingData((prevData) => ({
             ...prevData,
-            sets: prevData.sets.map((set, index) => 
-              index === 0 
-                ? { ...set, setTimeEnd: event.target.value } 
-                : set
-            ),
+            link: event.target.value,
           }));
     }
 
-    const handleCommentsInput = (event:ChangeEvent<HTMLTextAreaElement>) => {
-        setComments(event.target.value);
-    }
-
     const handleSubmit = () => {
+        // Will need to embed the setDetails in Sets and travelExpenses
         console.log(bookingData)
     }
 
+
+    const handleCommentsInput = (event:ChangeEvent<HTMLTextAreaElement>) => {
+        setComments(event.target.value);
+    } // IGNORE
+
     return (
-        <div className="absolute bg-[#20202A] text-[white] p-8 rounded-[20px] w-[40%] h-[89%]">
+        <div className="absolute bg-[#20202A] text-[white] p-8 rounded-[20px] w-[40%] h-[95%]">
             <h1 className={`${ubuntu.className} text-center text-2xl text-[#ccff69] tracking-[1px] mb-[15px]`}>Booking Request</h1>
-            <div className="h-[28%]">
+            <div className="h-[32%]">
             <span className="block text-[#ccff69] text-[11px] tracking-[1.5px] uppercase mt-[1px] mb-[25px] border-b-[#ccff69] border-b border-solid">Event Details</span>
             <div className="flex flex-col">
                 <div className="flex flex-row justify-center items-center w-[100%]">
@@ -170,6 +194,10 @@ export const BookNow: React.FC<BookNowProps> = ({ setOpenBooking }) => {
                         onChange={(e) => chooseDate(e)}
                         noOptionsMessage={noOptionsMessage}/>
                 </div>
+                <div className="flex flex-row justify-center items-center w-[100%]">
+                    <span className="rounded-[5px] ml-[-20px] text-center text-xs tracking-[1.5px] lowercase italic w-[25%]">website:</span>
+                    <input type="text" className="z-58 rounded-[5px] w-[55%] p-1 text-[black] text-center text-sm mb-[20px]" value={bookingData.link} onChange={handleWebsiteInput}/>
+                </div>
             </div>
             </div>
             <div className="h-[34%]">
@@ -187,36 +215,16 @@ export const BookNow: React.FC<BookNowProps> = ({ setOpenBooking }) => {
                         <div className="flex flex-row mt-[20px] mb-[23px] items-center">
                             <span className="rounded-[5px] text-center text-xs p-3 tracking-[1.5px] lowercase mt-[1px] italic ml-[5%] mr-[10px]">Pay Amount:</span>
                             <input type="number" value={offer} onChange={handleOfferInput} className="rounded-[5px] p-2 w-[19%] h-[80%] text-[black] text-center"/>
-                            <span className="rounded-[5px] ml-[30px] w-[95px] text-center text-xs p-3 tracking-[1.5px] lowercase mt-[1px] italic">Pay Type:</span>
-                            <Dropdown><DropdownTrigger>
-                                <span className="z-15 bg-[black] text-[white] py-3 uppercase text-[12px] px-[24px] rounded-[10px] tracking-[3px] transition-all duration-200 cursor-pointer">
-                                    {payType === '' ? 'Choose' : payType}
-                                </span>
-                            </DropdownTrigger>
-                            <DropdownMenu aria-label="Static Actions">
-                                <DropdownItem key="Hourly" className="text-center bg-[#20202A] p-3 hover:bg-[#3525de] shadow-[0px_4px_5px_#191922] transition-all duration-200 rounded-[10px_10px_0px_0px]" 
-                                onClick={() => handleTypeInput('Hourly')}>
-                                    <span className="text-[#b7c4ff] p-3 uppercase text-[12px] mx-[24px] my-[6px] tracking-[3px] transition-all duration-200">
-                                        Hourly
-                                    </span>
-                                </DropdownItem>
-                                <DropdownItem key="Flat" className="text-center bg-[#20202A] p-3 hover:bg-[#3525de] shadow-[0px_4px_5px_#191922] transition-all duration-200" onClick={() => handleTypeInput('Fixed')} >
-                                    <span className="text-[#b7c4ff] p-3 uppercase text-[12px] mx-[24px] my-[6px] tracking-[3px] transition-all duration-200">
-                                        Fixed
-                                    </span>
-                                </DropdownItem>
-                                <DropdownItem key="set" className="text-center bg-[#20202A] p-3 hover:bg-[#3525de] shadow-[0px_4px_5px_#191922] transition-all duration-200 rounded-[0px_0px_10px_10px]" onClick={() => handleTypeInput('Per Set')} >
-                                    <span className="text-[#b7c4ff] p-3 uppercase text-[12px] mx-[24px] my-[5px] tracking-[3px] transition-all duration-200">
-                                        Per Set
-                                    </span>
-                                </DropdownItem>
-                            </DropdownMenu></Dropdown>
+                            <span className="rounded-[5px] ml-[30px] w-[110px] text-center text-xs p-3 tracking-[1.5px] lowercase mt-[1px] italic">Travel Paid:</span>
+                            <span className="z-15 bg-[black] text-[white] py-3 uppercase text-[12px] px-[24px] rounded-[10px] tracking-[3px] transition-all duration-200 cursor-pointer" onClick={() => setTravelExpenses(!travelExpenses)}>
+                                {travelExpenses === false ? 'No' : 'Yes'}
+                            </span>
                         </div>
                     </div>
                 </div>
                 <span className="block text-[11px] tracking-[1.5px] uppercase mt-[1px] mb-[20px] border-b-[#ccff69] border-b border-solid"></span>
             </div>
-            <div className="h-[22%] flex justify-center p-4">
+            <div className="h-[20%] flex justify-center p-4">
                 <textarea className="m-3 rounded-[5px] w-[80%] p-3 text-[black] text-sm" placeholder="Add any extra details or comments here" value={comments} onChange={handleCommentsInput}></textarea>
             </div>
             <div className="flex flex-row justify-between mt-[10px]">
