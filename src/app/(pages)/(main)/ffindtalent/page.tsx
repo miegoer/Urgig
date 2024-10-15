bc232b4c31d9b5805b57af56b6eb96095dc1e3f6
Damir Lončarić <damir.loncaric987@gmail.com>
2024-10-15 21:12:27 +0200

    fixes...

--- src/app/(pages)/(main)/ffindtalent/page.tsx
+++
@@ -1,9 +0,0 @@
-import TalentSearch from "@/app/(components)/ui/findtalent/search";
-
-export default function FindTalent() {
-    return (
-        <>
-        <TalentSearch/>
-        </>
-    )
-  }
--- src/app/api/(DB)/users/[_id]/events/route.ts
+++ src/app/api/(DB)/users/[_id]/events/route.ts
@@ -1,4 +1,4 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { GETHelper } from "../getUserHelper";
 
 export async function GET(request: NextRequest, { params }: { params: { _id: string } }) {
