1cbc8df5780fea731504b14f2e66bbca7b1c6bc4
DiegoMHB <dmhb17@gmail.com>
2024-10-16 18:11:50 +0200

    starting point for testing

--- src/__test__/dashboard.test.tsx
+++ src/__test__/dashboard.test.tsx
@@ -10,41 +10,12 @@ jest.mock("next/navigation", () => ({
 }));
 
 
-jest.mock('@/app/(components)/ui/dashboard/quickstats', () => () => (
-  <div>QuickStats Component</div>
-));
-jest.mock('@/app/(components)/ui/dashboard/notifications', () => () => (
-  <div>Notifications Component</div>
-));
-jest.mock('@/app/(components)/ui/dashboard/messages', () => () => (
-  <div>Messages Component</div>
-));
-jest.mock('@/app/(components)/ui/dashboard/userinfo', () => () => (
-  <div>UserInfo Component</div>
-));
-jest.mock('@/app/(components)/ui/dashboard/EventsList', () => () => (
-  <div>EventsList Component</div>
-));
-
 
 
 describe("Dashboard", () => {
   it("should contain 5 different components", () => {
     render(<Page children = { null}/>);
 
-    expect(screen.getByText(/QuickStats/i)).toBeInTheDocument();
-    expect(screen.getByText(/Notifications/i)).toBeInTheDocument();
-    expect(screen.getByText(/Messages/i)).toBeInTheDocument();
-    expect(screen.getByText(/UserInfo/i)).toBeInTheDocument();
-    expect(screen.getByText(/EventsList/i)).toBeInTheDocument();
-      });
-
-  it("hallo", () => {
-    render(
-      // <TalkSessionContext.Provider value={{ session: ""}}>
-      <Page children={null} />
-    );
-    // </TalkSessionContext.Provider>)
 
 
   });
