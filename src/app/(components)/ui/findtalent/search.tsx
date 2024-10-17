5d0ae7871755c20f13608968ed7949ad61335eae
DiegoMHB <dmhb17@gmail.com>
2024-10-17 13:45:38 +0200

    filtering by location, and genre (with autocomplete) search button in the middle

--- src/app/(components)/ui/findtalent/search.tsx
+++ src/app/(components)/ui/findtalent/search.tsx
@@ -7,7 +7,7 @@
 // import SelectGenre from '../dashboard/selectGenre'; // Assuming this component fetches genres
 
 // export default function TalentSearch() {
-//     const [chosenGenres, setChosenGenres] = useState<string[]>([]); // Selected genres
+//     const [chosenGenres, setGenres] = useState<string[]>([]); // Selected genres
 //     const [locationSearch, setLocationSearch] = useState<string>(''); // Location search input
 //     const [isSent, setIsSent] = useState<boolean>(false); // To handle form submission or other logic
 
@@ -33,7 +33,7 @@
 //                         chosenGenres.map((genre) => (
 //                             <span
 //                                 key={genre}
-//                                 className="z-10 flex flex-wrap inline-flex text-s bg-[black] text-[#ffa01e] rounded-[20px] py-1 px-3 mr-2.5 mb-2.5 tracking-[1px]"
+//                                 className="z-10 flex flex-wrap  text-s bg-[black]  rounded-[20px] py-1 px-3 mr-2.5 mb-2.5 tracking-[1px]"
 //                             >
 //                                 {genre}
 //                             </span>
@@ -44,7 +44,7 @@
 //                 </div> */}
 
 //                 {/* Include the SelectGenre component to fetch genres */}
-//                 <SelectGenre setGenres={setChosenGenres} genres={chosenGenres} isSent={isSent} />
+//                 <SelectGenre setGenres={setGenres} genres={chosenGenres} />
 //             </form>
 
 //             <Image
@@ -67,51 +67,42 @@ import Search from "/public/search-icon.png";
 import Image from "next/image";
 import { musicGenres } from "@/mockData/musicGenres";
 import { User } from "@/types/user";
+import SelectGenre from "../dashboard/selectGenre";
 
-
-
-export default function TalentSearch({
-  artists,
-  setFilteredSearch,
-}: any) {
-
+export default function TalentSearch({ artists, setFilteredSearch }: any) {
   const [search, setSearch] = useState<string>("");
-  let [chosenGenres, setChosenGenres] = useState<string[]>([]);
-
+  let [genres, setGenres] = useState<string[]>([]);
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const filtered = artists.filter(
-      (artist: User) =>
-        search.length >= 4 &&
-        artist.name.toLowerCase().includes(search.toLowerCase())
+    const filtered = artists.filter((artist: User) =>
+      artist.location
+        ? search.length >= 4 &&
+          artist.location.toLowerCase().includes(search.toLowerCase())
+        : false
     );
     setFilteredSearch(filtered);
-    setSearch("")
+    setSearch("");
   };
 
   const chooseGenre = (genre: string) => {
-    if (chosenGenres.includes(genre)) return;
-    if (chosenGenres.length < 3) {
-      setChosenGenres((prev) => [...prev, genre]);
+    if (genres.includes(genre)) return;
+    if (genres.length < 3) {
+      setGenres((prev) => [...prev, genre]);
     } else {
       return;
     }
   };
 
-  useEffect(()=>{
-    const filtered = artists.filter(
-      (artist: User) =>{
-          if( !artist.profileDetails && !artist.profileDetails.genre) return false
-          return chosenGenres.every((genre) =>
-            artist.profileDetails.genre.includes(genre)
-          );
-      }
-    );
+  useEffect(() => {
+    const filtered = artists.filter((artist: User) => {
+      if (!artist.profileDetails && !artist.profileDetails.genre) return false;
+      return genres.every((genre) =>
+        artist.profileDetails.genre.includes(genre)
+      );
+    });
     setFilteredSearch(filtered);
-
-  },[chosenGenres])
-
+  }, [genres]);
 
   return (
     <div
@@ -119,10 +110,9 @@ export default function TalentSearch({
         background:
           "linear-gradient(355deg, rgba(32,33,54,1) 0%, rgba(52,41,98,1) 100%)",
       }}
-      className="mt-2 rounded-[20px] text-center bg-[#252531] shadow-[0px_0px_0px_#272525] p-4"
+      className="mt-2 rounded-[20px] text-center bg-[#252531] shadow-[0px_0px_0px_#272525] p-3"
     >
-      <div>
-        <form onSubmit={onSubmit}>
+        <form onSubmit={onSubmit} className="p-2">
           <span className="block text-[11px] tracking-[1px] uppercase ml-2.5 mr-0 mt-[7px] mb-[10px]">
             Artist's name
           </span>
@@ -131,24 +121,25 @@ export default function TalentSearch({
               type="text"
               onChange={(e) => setSearch(e.target.value)}
               value={search}
-              className="rounded-[20px] text-[black] text-center py-[5px] px-[10px] text-sm"
+              className="rounded-[20px] text-[black] text-center py-[5px]  text-sm h-[2.5rem] w-full"
             />
-            <button>
-              <Image
-                src={Search}
-                width={50}
-                height={50}
-                alt="search icon"
-                className="m-auto mb-4 mt-4 rounded-[50%] p-1 opacity-90"
-                style={{ background: "rgba(255,160,30,1) 100%" }}
-              />
-            </button>
           </div>
+          <span className="block text-[11px] tracking-[1px] uppercase ml-2.5 mr-0 mt-[20px] mb-[10px] ">
+            Genres
+          </span>
+          <SelectGenre setGenres={setGenres} genres={genres}/>
+          <button>
+            <Image
+              src={Search}
+              width={50}
+              height={50}
+              alt="search icon"
+              className="m-auto mb-4 mt-4 rounded-[50%] p-1 opacity-90"
+              style={{ background: "rgba(255,160,30,1) 100%" }}
+            />
+          </button>
         </form>
-        <span className="block text-[11px] tracking-[1px] uppercase ml-2.5 mr-0 mt-[20px] mb-[10px]">
-          Genres
-        </span>
-        <div className="dropdown relative block">
+        {/* <div className="dropdown relative block">
           <button className="dropbtn bg-[#ccff69] text-[black] text-xs px-4 py-2 border-[none] rounded-[20px]">
             Choose Genre Tags
           </button>
@@ -180,7 +171,7 @@ export default function TalentSearch({
                 <span>{genre}</span>
                 <span
                   onClick={(e) => {
-                    setChosenGenres((prev) => {
+                    setGenres((prev) => {
                       const newGenres = [...prev];
                       newGenres.splice(index, 1);
                       return newGenres;
@@ -194,8 +185,8 @@ export default function TalentSearch({
           ) : (
             <span className="text-xs italic ">No genres added</span>
           )}
-        </div>
+        </div> */}
       </div>
-    </div>
+    
   );
 }
